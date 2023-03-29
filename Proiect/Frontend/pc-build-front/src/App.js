ca46fad7e2195001d3843acad10ef8568d52cbc4
915-Mocanu-Andrei <72067795+AndreiM29@users.noreply.github.com>
2023-03-30 00:03:04 +0300

    add authentication to the React app

--- Proiect/Frontend/pc-build-front/src/App.js
+++ Proiect/Frontend/pc-build-front/src/App.js
@@ -1,5 +1,27 @@
-import logo from './logo.svg';
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
+  return (
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
+
+/*import logo from './logo.svg';
 import './App.css';
+import { Amplify, Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -22,4 +44,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;*/
